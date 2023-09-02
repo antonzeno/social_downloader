2c2ff1b2067d6f44fee1a031ea748bed54729ad4
Andon Zeno <tonizeno5@gmail.com>
2023-09-02 20:11:38 +0200

    Setup TS, Express & Dependencies

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use bodyParser to parse JSON and form data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable CORS to allow cross-origin requests
+app.use(
+    cors({
+        credentials: true,
+    })
+);
+// Use compression to compress responses
+app.use(compression());
+app.use(cookieParser());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
